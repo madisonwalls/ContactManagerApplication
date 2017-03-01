3fdd1a9ce926dabc92d07f453dad907c00d62127
madisonwalls <madilyn@live.unc.edu>
2017-02-28 22:06:44 -0500

    Added upload file for image purposes.

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
   $tasks = $db->query('SELECT * FROM tasks')->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
 
 <?php include 'footer.php'; ?>
